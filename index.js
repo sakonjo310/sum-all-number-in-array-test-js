cfae9255896b5804fd82009d99c61eea8f6f469a
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-24 02:48:21 +0000

    Add function to sum array of nested arrays of numers

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      // console.log(item)
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    } 
+  })
+  return sum;
 }
-
+// console.log(sumItems([1,2,[3,4],5]))
 module.exports = sumItems;
